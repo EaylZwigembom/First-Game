50d660f9bbee3c91cfcca3ab78a685422b67e552
EaylZwigembom <124462589+EaylZwigembom@users.noreply.github.com>
2023-07-28 13:11:54 +0300

    Update MovementScript.cs
    Jsusjs7ahbwan

--- First Game zwisch/Assets/Scripts/MovementScript.cs
+++ First Game zwisch/Assets/Scripts/MovementScript.cs
@@ -1,21 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
-    public Rigidbody rb;
-    public float speed;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float speed;
     
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         if (Input.GetKey("w")){
-        rb.AddForce(0,0, speed * Time.deltaTime, ForceMode.VelocityChange);
+        rb.AddForce(0,0, speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
     
     }
 
             if (Input.GetKey("s")){
-        rb.AddForce(0,0, -speed * Time.deltaTime, ForceMode.VelocityChange);
+        rb.AddForce(0,0, -speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
     
     }
 }
